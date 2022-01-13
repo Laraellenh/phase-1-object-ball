ec82bdae8f5cd19aa22179f665b38f00e5f87f45
Lara <laraellenh@gmail.com>
2022-01-13 10:43:38 -0500

    your turn to un F* the away data

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -1,25 +1,4 @@
-// make object (function) gameObject
-// - The top level of the object has two keys: `"home"`, for the home team, and
-// `"away"`, for the away team.
-// - The values of the `"home"` and `"away"` keys are objects too. These objects
-// have the following keys:
-// - `"teamName"`
-// - `"colors"`
-// - `"players"`
-// - The `teamName` key points to a string of the team name.
-// - The `colors` key points to an array of strings that are that team's colors.
-// - The `players` key points to an object of players whose names (as strings) are
-// the keys to a object containing their stats. The values for each player's
-// names and their stats can be found in the table below. The stats keys should
-// be formatted like this:
-// - `"number"`
-// - `"shoe"`
-// - `"points"`
-// - `"rebounds"`
-// - `"assists"`
-// - `"steals"`
-// - `"blocks"`
-// - `"slamDunks"`
+
 
 const gameObject = () => ({
     home: {
@@ -75,12 +54,65 @@ const gameObject = () => ({
                 blocks: "11",
                 slamDunks: "1",
             },
-            // away: {
+    away: {
 
-            // teamName: "",
-            // colors: "",
-            // players: "",
-        }
-
-    })
-}
+        teamName: "Charlotte Hornets",
+        colors: "Turquoise, Purple",
+        players: {
+            "Jeff Adrien": {
+                number: "4",
+                shoe: "18",
+                points: "10",
+                rebounds: "1",
+                assists: "1",
+                steals: "2",
+                blocks: "7",
+                slamDunks: "2",
+            }, "Bismak Biyombo": {
+                number: "0",
+                shoe: "16",
+                points: "12",
+                rebounds: "4",
+                assists: "7",
+                steals: "7",
+                blocks: "15",
+                slamDunks: "10",
+            }, "DeSanga Diop": {
+                number: "2",
+                shoe: "14",
+                points: "22",
+                rebounds: "12",
+                assists: "12",
+                steals: "4",
+                blocks: "5",
+                slamDunks: "5",
+            }, "Ben Gordon": {
+                number: "8",
+                shoe: "15",
+                points: "33",
+                rebounds: "3",
+                assists: "2",
+                steals: "1",
+                blocks: "1",
+                slamDunks: "0",
+            },
+                "Brendan Haywood ": {
+                number: "33",
+                shoe: "15",
+                points: "6",
+                rebounds: "12",
+                assists: "12",
+                steals: "22",
+                blocks: "5",
+                slamDunks: "12",
+            },
+        }}
+    }}
+});
+gameObject()
+  
+  function awayTeamName() {
+      let object = gameObject()
+      return object['away']['teamName']
+  }
+  console.log(awayTeamName())
